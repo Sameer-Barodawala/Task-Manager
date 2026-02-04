a669d93078e3a4d082e2344501b8e98d9670c0c9
Sameer <sameerbarodawala@gmail.com>
2026-02-04 18:32:00 +0530

    Adjusted the db.js To work with Render

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,15 +2,26 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    ssl: {
+    rejectUnauthorized: false
+  }
 });
 
+pool.getConnection()
+  .then(conn => {
+      console.log('✅ MySQL connected successfully');
+      conn.release();
+  })
+  .catch(err => {
+      console.error('❌ MySQL connection failed:', err);
+  });
 const promisePool = pool.promise();
 
 module.exports = promisePool;
