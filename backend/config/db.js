279bf22c4b302862d0108323a729fafe83047b07
Sameer <sameerbarodawala@gmail.com>
2026-02-04 19:18:03 +0530

    Adjusted the db.js To work with Render

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ console.log('ENV CHECK:', {
   MYSQL_HOST: process.env.MYSQL_HOST,
   MYSQL_USER: process.env.MYSQL_USER,
   MYSQL_DATABASE: process.env.MYSQL_DATABASE,
-  MYSQL_PORT: process.env.MYSQL_PORT
+  MYSQL_PORT: process.env.MYSQLPORT
 });
 
 const pool = mysql.createPool({
